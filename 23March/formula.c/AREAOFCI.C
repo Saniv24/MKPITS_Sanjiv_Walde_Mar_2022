51155528c338d1502a6801bdbbef776c01f04380
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-03 17:08:13 +0530

    area of circle

---
+++ 23March/formula.c/AREAOFCI.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+float area, r;
+float pi=3.14;
+printf("Enter the value of radius:");
+scanf("%f", &r);
+area=pi*r*r;
+printf("area=%f", area);
+}
