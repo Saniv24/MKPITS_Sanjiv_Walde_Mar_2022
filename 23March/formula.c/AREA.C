ff59f71fcba4b3ddae272f41830c12b51fa85554
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-03 17:07:29 +0530

    area of square

---
+++ 23March/formula.c/AREA.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+float area, length, width;
+printf("Enter Length and width");
+scanf("%f %f", &length, &width);
+area=width*length;
+printf("area=%f",area);
+}
