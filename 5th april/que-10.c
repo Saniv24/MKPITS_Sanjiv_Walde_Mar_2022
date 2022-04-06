f540adfed994dea924373ca0766fc3b180ea1990
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-06 12:52:56 +0530

    sum , get integer from user

---
+++ 5th april/que-10.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <conio.h>
+ void main() 
+ {
+    int num1, num2, sum;
+    printf("Enter num1:");
+    scanf("%d",&num1);
+    printf("Enter num2:");
+    scanf("%d",&num2);
+    
+    sum=num1+num2;
+    printf("Sum of num1 and num2 is: %d",sum);
+    
+ }
+ 
