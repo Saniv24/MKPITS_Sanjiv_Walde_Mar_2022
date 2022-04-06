3cbe64a05e14975fa363b7f6da3e667a1d2ce528
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-06 12:40:49 +0530

    sum of 2 no.

---
+++ 5th april/que-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, sum;
+    printf("\n Enter first number: ");
+    scanf("%d",&a);
+    printf(" Enter the second number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf(" Sum of a and b is: %d",sum);
+}
