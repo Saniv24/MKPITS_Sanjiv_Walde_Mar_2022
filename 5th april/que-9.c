2d18f2675b64348768cd5fa0a66cf959462947d9
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-06 12:51:02 +0530

    days into year & week

---
+++ 5th april/que-9.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <conio.h>
+ void main() 
+ {
+    int days, years, weeks;
+    printf("Enter days here:\n");
+    scanf("%d",&days);
+    years = days/365; 
+    weeks = (days % 365)/7;
+    days = days- ((years*365) + (weeks*7));
+
+    printf("Years: %d\n", years);
+    printf("Weeks: %d\n", weeks);
+    printf("Days: %d \n", days);
+ }
+ 
