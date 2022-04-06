70e5cfdcff5b832346749eaf45e4adb8a8f0fce5
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-06 12:43:32 +0530

    perimeter & area of rectangle

---
+++ 5th april/que-7.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <conio.h>
+ int main() 
+ {          
+   int h=7, w=5, perimeter, area;
+    /*considering h='l';*/
+   perimeter=2*(w+h);
+   
+   printf("perimeter of an rectangle is %d:\n \n",perimeter);
+   
+   
+    area=h*w;
+    printf("Area of rectangle is %d:",area);
+ }
+ 
