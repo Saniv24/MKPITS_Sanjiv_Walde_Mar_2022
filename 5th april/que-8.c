cdb1b98821a1f18f4fd4ec8f15fc3c73d16cf48f
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-06 12:45:27 +0530

    perimeter & area of circle

---
+++ 5th april/que-8.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <conio.h>
+ int main() 
+ {          
+   float  perimeter, area;
+   float  pi=3.14;
+   float  r=6;
+   
+   perimeter=2*pi*r;
+   
+   printf("perimeter of an rectangle is %f:\n \n",perimeter);
+   
+   
+   area=pi*r*r;
+   printf("Area of circle is: %f",area);
+ }
+ 
