9271875d43d238cd74d365057815a0fedb1a8567
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-11 21:37:23 +0530

    dividing two no.

---
+++ 5th april/que-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <conio.h>
+ void main() 
+ {
+     int a, b, Division;
+     printf("Enter 1st number:");
+     scanf("%d",&a);
+     printf("Enter 2nd number:");
+     scanf("%d",&b);
+     
+     Division=a/b;
+     printf("Division of two number is: %d",Division);
+ }
+ 
