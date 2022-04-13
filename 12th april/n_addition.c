5da1b3006ce95e0d00ccd3b7f2697bb4933387c7
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-13 13:44:06 +0530

    class work

---
+++ 12th april/n_addition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() 
+{
+  int n, r, sum=0;
+  printf(" Enter number:");
+  scanf("%d",&n);
+  
+  while(n>0)
+  {
+	  r=n%10;
+	   sum=sum+r;
+	   n=n/10;
+  } 
+  
+        printf("Sum of all no is: %d",sum);
+}
