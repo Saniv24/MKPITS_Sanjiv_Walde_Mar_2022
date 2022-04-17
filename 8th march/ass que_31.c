8857f1ee13a75b0c54fa0404c6410a07f6c99e73
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-17 15:00:43 +0530

    ascending descending order

---
+++ 8th march/ass que_31.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+void main() 
+{
+   int a, b;
+   printf("Entar two pair values:");
+   scanf("%d%d",&a, &b);
+   
+   if(a>b)
+   {
+       printf("Descending order");
+   }
+   else
+   {
+       printf("Ascending order");
+   }
+}
+
+
+
