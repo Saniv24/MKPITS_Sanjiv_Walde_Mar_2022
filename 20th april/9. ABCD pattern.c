0069aedb846f51452815c744702145c67879faf9
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-21 13:43:37 +0530

    square alphabet pattern

---
+++ 20th april/9. ABCD pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int r, c;
+	
+	for(r=1; r<=3; r++)
+	{
+	    char alpha='A';
+		for(c=1; c<=4; c++)
+		{
+			printf("%c",alpha);
+			alpha++;
+		}
+	printf("\n");
+	}
+	
+}
