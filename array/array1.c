5dad4d9f9d2b6d9d3c963ad2dc21d5286e4e2cfa
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-21 18:18:42 +0530

    print value using loop in array

---
+++ array/array1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+  
+int main()
+{
+    int arr[5];
+    arr[0] = 5;
+    arr[2] = 10;
+    arr[3] = 2; 
+    arr[4] = 4;
+  
+    printf("%d\n%d\n%d\n%d\n", arr[0], 
+           arr[1], arr[2], arr[3]);
+  
+    return 0;
+}
