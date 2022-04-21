4e44c7d38fdac219614360fd2e97d2c431a17bd3
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-21 13:44:40 +0530

    number pattern

---
+++ 20th april/12.number pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int r, c;
+
+    for(r=1; r<=3; r++)
+    {
+        
+        for(c=r; c<3; c++)
+        {
+            printf(" ");
+        }
+        for(c=1; c<=r; c++)
+        {
+            printf("%d", c);
+        }
+
+        printf("\n");
+    }
+}
