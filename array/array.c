994e3cee9321c0fe983c49079c5d5b9a51e9e1bd
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-04-21 18:18:08 +0530

    print simple value

--- array/array.c
+++ array/array.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+void main()
+{
+	int a[3];
+	int i;
+	printf("Enter any 3 number:");
+	for(i=0;i<=2;i++)
+	{
+		
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<=2;i++)
+	{
+		printf("a[%d]=%d\n",i,a[i]);
+	}
+}
