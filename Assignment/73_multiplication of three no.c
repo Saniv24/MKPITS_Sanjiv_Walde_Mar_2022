a1aca1b519de8ea13b48ea4492adb42d16b65a6b
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-02 10:47:13 +0530

    Assignment

---
+++ Assignment/73_multiplication of three no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c, mul;
+	
+	printf("Enter 1st,2nd, 3rd no.:");
+
+	scanf("%d%d%d", &a,&b,&c);
+	
+	mul=a*b*c;
+	
+	printf("Multiplication of three no is:%d",mul);
+}
