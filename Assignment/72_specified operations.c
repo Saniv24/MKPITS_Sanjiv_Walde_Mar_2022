82dfea41f8c5f7d99739a7a113a5b1c5386a39f2
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-02 11:27:31 +0530

    Assignment

---
+++ Assignment/72_specified operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a, b, c, d;
+	a=-1+4*6;
+	b=(35+5)%7;
+	c=14+(-4*6)/11;
+	d=2+15/6*1-7%2;
+	
+	printf("a=%d\nb=%d\nc=%d\nd=%d\n",a, b, c, d);
+}
