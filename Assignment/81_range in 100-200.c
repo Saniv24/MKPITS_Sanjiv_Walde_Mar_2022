ab91d4a76108a3c145f7fb7db11df1006f858fa9
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-02 10:42:11 +0530

    Assignment

---
+++ Assignment/81_range in 100-200.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	printf("enter any integer:");
+	scanf("%d",&a);
+	
+	if(100<=a && 200>=a)
+	{
+		printf("No is in range");
+	}
+	else 
+	{
+		printf("No is not in range");
+	}
+}
+
+	
