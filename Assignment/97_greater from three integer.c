455e5e8bb697898d24d6c6b3d4b2f8d886c79170
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-09 11:08:22 +0530

    Assignment

---
+++ Assignment/97_greater from three integer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x, y, z;
+	
+	printf("Enter value of z, y 7 z:\n\n");
+	scanf("%d%d%d",&x, &y, &z);
+	 if(y>x && z>y)
+	 {
+	 	printf("1");
+	 }
+	 else
+	 {
+	 	printf("0");
+	 }
+}
