77f66d05a458e2a4a5e89068296511f502235f0d
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-09 11:14:27 +0530

    Assignment

---
+++ array/66_integer equivalent of letters.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char i;
+    printf(" a-z:\n");
+    for(i=65; i<=90; i++)
+    printf(" %d",i);
+    
+    printf("\n A-Z\n");
+    for(i=97; i<=122; i++)
+    {
+        printf(" %d",i);
+    }
+}
