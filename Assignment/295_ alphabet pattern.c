8ded0ce9dbe26c4634b83f5a836511bda449473e
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-17 11:56:45 +0530

    Assignment

---
+++ Assignment/295_ alphabet pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+     int r, c;
+    for(r=69;r>=65;r--)
+    {
+        
+        for(c=65;c<=r;c++)
+        {
+            printf("%c",r);
+            
+        }
+        printf("\n");
+    }
+}
+
+
