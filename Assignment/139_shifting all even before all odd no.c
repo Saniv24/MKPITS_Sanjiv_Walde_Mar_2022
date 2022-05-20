8bc320a73456ec123e32b3606911a44d831d9f09
Saniv24 <101799061+Saniv24@users.noreply.github.com>
2022-05-20 11:53:12 +0530

    assignment

---
+++ Assignment/139_shifting all even before all odd no.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[20], i, x=0, temp;
+    printf("Enter values :");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        if(a[i]%2==0)
+        {
+            temp=a[x];
+            a[x]=a[i];
+            a[i]=temp;
+            x=x+1;
+        }
+    }
+    printf("New array is :");
+    for(i=0;i<5;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
